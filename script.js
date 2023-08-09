bb55afb3f5081c5982f1fedcdc9f84d1d99815c1
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2023-08-09 20:50:05 +0300

    fixed 0 bug

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 window.onload = function () {
     const button = document.getElementById("button1");
     const numberTitle = document.getElementById("number");
-    var number = 100;
+    var number = 10;
     numberTitle.innerHTML = number;
     button.addEventListener("click", myFunction);
 
     function myFunction() {
-        number--;
-        numberTitle.innerHTML = number;
-
+        if (number == 0) {
+            numberTitle.innerHTML = "TEBRIK EDERIM KAZANDINIZ"
+        } else if (number == -1) {
+            return
+        } else {
+            number--;
+            numberTitle.innerHTML = number;
+        }
     }
 }
